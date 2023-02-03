f4f93bd3757e1d95e4a4155c012390196a20ee3e
Fabio <fabio.bernasconi8@gmail.com>
2023-02-04 09:18:03 +1300

    fixed help commands

--- HomeworkBot.py
+++ HomeworkBot.py
@@ -35,40 +35,55 @@ def LoadJSON():
     homework=json.loads(importedJson.read())
     importedJson.close()
 
+def getNextWkDay():
+    year = datetime.now().year
+    today = datetime(year, datetime.now().month, datetime.now().day)
+    wkDay = today.strftime('%a')
+    if today.strftime('%a') == 'Fri':
+        nextWkDay = today+timedelta(days=3)
+    elif today.strftime('%a') == 'Sat':
+        nextWkDay = today+timedelta(days=2)
+    else:
+        nextWkDay = today+timedelta(days=1)
+    return nextWkDay
+
+
 fullRole=f'<@&{1067180148702593135}>'
 
 def SendMessage():
     global homework
     LoadJSON()
 
-    dueTomorow=[]
+    dueNextWkDay=[]
     dueAfter=[]
     year=datetime.now().year
     today=datetime(year,datetime.now().month,datetime.now().day)
     tomorow=today+timedelta(days=1)
+    nextWkDay = getNextWkDay()
     modifier=0
 
     for i in range(len(homework)):
         curI=homework[i-modifier]
         due=datetime(year,int(curI["dueMonth"]),int(curI["dueDay"]))
+        
         subject=names[curI["subject"]]
-        if due==tomorow:
-            dueTomorow.append(f"> **{subject}** {curI['txt']}")
-        elif due>tomorow:
-            dueAfter.append(f"> `{int(curI['dueDay']):02}/{int(curI['dueMonth']):02}` **{subject}** {curI['txt']}")
-        else:
+        if due == nextWkDay:
+            dueNextWkDay.append(f"> **{subject}** {curI['txt']}")
+        elif due < tomorow:
             homework.pop(i)
             modifier+=1
-    
+        else:
+            dueAfter.append(f"> `{int(curI['dueDay']):02}/{int(curI['dueMonth']):02}` **{subject}** {curI['txt']}")
+        
     SaveJSON()
 
-    message=f'{fullRole} due {tomorow.strftime("%A, %d %b %Y")}:\n'
-    if len(dueTomorow)==0:
+    message=f'{fullRole} due {nextWkDay.strftime("%A, %d %b")}:\n'
+    if len(dueNextWkDay)==0:
         message=f'{message}> None\n'
-    for hwItem in dueTomorow:
+    for hwItem in dueNextWkDay:
         message=f'{message}{hwItem}\n'
     if len(dueAfter)>0:
-        message=f'{message}\nHomework due after {tomorow.strftime("%A, %d %b %Y")}:\n'
+        message=f'{message}\nOther homework due:\n'
         for hwItem in dueAfter:
             message=f'{message}{hwItem}\n'
     message=f'{message}\nPlease message me if I have missed something and put questions in the <#1068030623610052638>.'
@@ -93,19 +108,27 @@ async def on_ready():
 
 @client.command()
 async def add(ctx, *, added):
-    added = added.split(',')
-    print(added)
-    tDate = added[0].split('/')
-    print(tDate)
-    year=datetime.now().year
-    today=datetime(year,datetime.now().month,datetime.now().day)
-    tomorow=today+timedelta(days=1)
-    modifier=0
 
     LoadJSON()
 
+    added = added.split(',',2)
+    print(added)
+    tDate = added[0].split('/')
+    print(tDate)
+
+    year=datetime.now().year
+    try:
+        tmp = datetime(year,int(tDate[1]),int(tDate[0]))
+        goodDate = True
+    except:
+        goodDate = False
+
     if len(added) != 3 or len(tDate) != 2:
         await ctx.send("**Error**: Please add with the following format. `day/month, subject, task`")
+    elif added[1].strip() not in names:
+        await ctx.send("**Error**: Please enter a valid **subject** with the following format. `day/month, subject, task` For more info see `!help subjects`.")
+    elif not goodDate:
+        await ctx.send("**Error**: Please enter a valid **date** with the following format. `*day/month*, subject, task`")
     else:
         homework.append({})
         homework[-1]['subject'] = added[1].strip()
@@ -123,8 +146,12 @@ async def send(ctx):
 @client.command()
 async def test(ctx):
     channel=client.get_channel(1067180194537930844)
+    nextWkDay = getNextWkDay()
+    nWkDShort = nextWkDay.strftime("%d/%m")
+    nWkDLong = nextWkDay.strftime("%d of %b").lstrip("0")
     msg = SendMessage().replace(fullRole,'<Role>')
     embed = discord.Embed(title = "Homework", description = msg)
+    embed.add_field(name='**Syntax**', value=f'There are 3 parameters:\n> **Date**: Day & Month: `{nWkDShort}`({nWkDLong})\n> **Subject**: Signified by 3/4 letters. Do `!help subjects` for list of subjects.\n> **Task**: E.g. `Scipad pg 222`')
     await ctx.send(embed = embed)
 
 #help commadnds
@@ -134,18 +161,64 @@ async def help(ctx):
     embed = discord.Embed(title="Help", description="Use !help <command> for more info")
 
     embed.add_field(name='Adding Hw', value='`add`')
-    embed.add_field(name='Sending Hw', value='`send/test`')
+    embed.add_field(name='Subject Keys', value='`subjects`')
+    embed.add_field(name='Checking Hw', value='`test`')
+    embed.add_field(name='Sending Hw', value='`send`')
 
     await ctx.send(embed=embed)
 
 @help.command()
 async def add(ctx):
+    year=datetime.now().year
+    today=datetime(year,datetime.now().month,datetime.now().day)
+    nextWkDay = getNextWkDay()
+    nWkDShort = nextWkDay.strftime("%d/%m")
+    nWkDLong = nextWkDay.strftime("%d of %b").lstrip("0")
+    
+    #Embed(add)
     embed = discord.Embed(title="Help: Add", description="This command adds items of homework to the list.")
 
-    embed.add_field(name='**Syntax**', value='There are 3 parameters:\n> Date: Day & Month: `9/2`(9th of Feb)\n\n> Subject: Signified by 3/4 letters. Do !help subjects for list of subjects.\n\n> Task: E.g. `Scipad pg 222`')
+    embed.add_field(name='**Syntax**', value=f'There are 3 parameters:\n> **Date**: Day & Month: `{nWkDShort}`({nWkDLong})\n> **Subject**: Signified by 3/4 letters. Do `!help subjects` for list of subjects.\n> **Task**: E.g. `Scipad pg 222`')
 
-    embed.add_field(name='**Example Usage**', value='If you want to add Scipad pg 222 for Science on the 9th of Feb then you would type: `!add 9/2, sci, Scipad pg 222')
+    embed.add_field(name='**Example Usage**', value=f'If you want to add Scipad pg 222 for Science on the {nWkDLong} then you would type: `!add {nWkDShort}, sci, Scipad pg 222`')
     await ctx.send(embed=embed)
 
+#subjects
+@help.command()
+async def subjects(ctx):
+    embed = discord.Embed(title="Help: Subject Codes", description="This command shows all the subject codes used in the add command.")
+
+    embed.add_field(name='**Syntax**', value=f'''
+    > Tutor: `tut`
+> 
+    > Chinese: `chi`
+    > Spanish: `spa`
+    > 
+    > English: `eng`
+    > Humanities: `hum`
+> 
+    > Maths: `mat`
+    > Science: `sci`
+> 
+    > Advanced Maths: `amat`
+    > Advanced Science: `asci`
+> 
+    > Music: `mus`
+> 
+    > Art: `art`
+    ''')
+    await ctx.send(embed=embed)
+
+
+#test
+@help.command()
+async def test(ctx):
+    embed = discord.Embed(title="Help: Test", description="This sends the homework message in the channel you are currently in are removes the ping.")    
+
+
+#send
+@help.command()
+async def send(ctx):
+    embed = discord.Embed(title="Help: Send", description="This sends the homework message in the homework channel and pings the homework role.")  
 
 client.run(TOKEN)
